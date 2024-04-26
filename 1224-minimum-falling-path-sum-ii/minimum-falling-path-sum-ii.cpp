0f5b21101ef0bdc00fa54a1ce0482209046dd841
Rishita Srivastava <104610927+Rishita1304@users.noreply.github.com>
2024-04-26 17:11:20 +0530

    Time: 107 ms (70.37%) | Memory: 17.1 MB (78.75%) - LeetSync

--- 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
@@ -2,22 +2,22 @@ class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& grid) {
         int n = grid.size();
-        vector<vector<int>> dp(n, vector<int> (n,-1));
+        vector<vector<int>> dp(2, vector<int> (n,-1));
         for(int j=0;j<n;j++) dp[0][j] = grid[0][j];
 
         for(int i=1;i<n;i++){
             for(int j=0;j<n;j++){
-                dp[i][j] = grid[i][j];
+                dp[i%2][j] = grid[i][j];
                 int min_prev = INT_MAX;
                 for(int k=0;k<n;k++){
-                    if(k!=j) min_prev = min(min_prev, dp[i-1][k]);
+                    if(k!=j) min_prev = min(min_prev, dp[(i-1)%2][k]);
                 }
-                dp[i][j] += min_prev;
+                dp[i%2][j] += min_prev;
             }
         }
         int ans = INT_MAX;
         for(int j=0;j<n;j++){
-            ans = min(ans, dp[n-1][j]);
+            ans = min(ans, dp[(n-1)%2][j]);
         }
         return ans;
     }
