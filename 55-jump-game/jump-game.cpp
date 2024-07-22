89342f861ab7dd550b922e5eb49909a442a36ffc
Rishita Srivastava <rishitasrivastava04@gmail.com>
2024-07-22 14:39:20 +0530

    Time: 43 ms (65.74%) | Memory: 51.1 MB (23.25%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int j = n-1;
+        for(int i=j;i>=0;i--){
+            if(i+nums[i]>=j) j=i;
+        } 
+        return j==0;
+    }
+};
