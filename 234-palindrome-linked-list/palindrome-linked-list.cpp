b7cd7f20fd503c3286b637361a4e5f97372a2b3d
Rishita Srivastava <rishitasrivastava04@gmail.com>
2024-08-16 02:38:12 +0530

    Time: 150 ms (86.70%) | Memory: 116.9 MB (76.74%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -10,18 +10,9 @@
  */
 class Solution {
 public:
-    ListNode* getMiddle(ListNode* head){
-        ListNode* slow = head;
-        ListNode* fast = head->next;
-        while(fast && fast->next){
-            slow=slow->next;
-            fast=fast->next->next;
-        }
-        return slow;
-    }
     ListNode* reverse(ListNode* head){
-        ListNode* curr = head;
         ListNode* prev = NULL;
+        ListNode* curr = head;
         ListNode* next = NULL;
         while(curr){
             next = curr->next;
@@ -32,20 +23,17 @@ public:
         return prev;
     }
     bool isPalindrome(ListNode* head) {
-        if(!head || !head->next){
-            return true;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast && fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
         }
-        ListNode* middle = getMiddle(head);
-        ListNode* temp = middle->next;
-        middle->next = reverse(temp);
-        ListNode* head1 = head;
-        ListNode* head2 = middle->next;
-        while(head2){
-            if(head1->val != head2->val){
-                return false;
-            }
-            head1=head1->next;
-            head2=head2->next;
+        ListNode* middle = reverse(slow);
+        while(middle){
+            if(head->val != middle->val) return false;
+            head = head->next;
+            middle = middle->next;
         }
         return true;
     }
