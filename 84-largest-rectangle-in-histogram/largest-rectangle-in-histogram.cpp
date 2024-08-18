14e22fe57b375b4aefecac9a6067acc4ec99a1d9
Rishita Srivastava <rishitasrivastava04@gmail.com>
2024-08-19 01:21:29 +0530

    Time: 138 ms (19.99%) | Memory: 90.5 MB (18.54%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
     vector<int> nextSmaller(vector<int>& heights) {
-        int n=heights.size();
-        stack<int> s;
-        vector<int> ans(n);
-        s.push(-1);
+        int n=heights.size(); vector<int> ans(n);
+        stack<int> s; s.push(-1);
+
         for(int i=n-1;i>=0;i--){
             int cur = heights[i];
-            while(s.top() != -1 && cur<=heights[s.top()]){
-                s.pop();
-            }
+            while(s.top() != -1 && cur<=heights[s.top()]) s.pop();            
             ans[i] = s.top();
             s.push(i);
         }
         return ans;
     };
         vector<int> prevSmaller(vector<int>& heights ) {
-        int n=heights.size();
-        stack<int> s;
-        vector<int> ans(n);
-        s.push(-1);
+        int n=heights.size(); vector<int> ans(n);
+        stack<int> s; s.push(-1);
+
         for(int i=0;i<n;i++){
             int cur = heights[i];
-            while(s.top() != -1 && cur<=heights[s.top()]){
-                s.pop();
-            }
+            while(s.top() != -1 && cur<=heights[s.top()]) s.pop();
             ans[i] = s.top();
             s.push(i);
         }
@@ -37,11 +31,11 @@ public:
         vector<int> prev(n,0), next(n,0);
         prev = prevSmaller(heights);
         next = nextSmaller(heights);
+
         for(int i=0;i<n;i++){
+            if(next[i]==-1) next[i]=n;  
+
             int l = heights[i];
-            if(next[i]==-1){
-                next[i]=n;
-            }
             int b = next[i]-prev[i]-1;
             int newArea = l*b; 
             ans = max(ans, newArea);
