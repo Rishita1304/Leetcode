c84ba3989c0eb86baf2b4795cb095bbb4da21c1f
Rishita Srivastava <rishitasrivastava04@gmail.com>
2024-08-19 01:04:22 +0530

    Time: 8 ms (74.06%) | Memory: 20.9 MB (13.51%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& a) {
+        int n = a.size();
+        stack<int> s;
+
+        for(int i=0;i<n;i++){
+            if(s.empty() || a[i]>0) s.push(a[i]); //empty or +ve curr value
+            else{ // -ve curr value
+                while(!s.empty() && s.top()>0 && s.top()<abs(a[i])) 
+                    s.pop(); //stack filled and top +ve and cur -ve bigger
+                if(!s.empty() && s.top()==abs(a[i])) s.pop(); //top and curr equal opp
+                else if(s.empty() || s.top()<0) s.push(a[i]); 
+            }
+        }
+        vector<int> ans;
+        while(!s.empty()){
+            ans.push_back(s.top()); s.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
