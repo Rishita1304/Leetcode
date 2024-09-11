edfe2b585924ec89204546117e60d78ca0d5a029
Rishita Srivastava <rishitasrivastava04@gmail.com>
2024-09-11 19:42:37 +0530

    Time: 3 ms (97.11%) | Memory: 28.5 MB (58.30%) - LeetSync

---
+++ 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int ind = 0;
+    int search(vector<int>& post, int start, int end, int temp){
+        for(int i=start ;i<=end ;i++) if(post[i] == temp) return i;
+        return -1;
+    }
+    TreeNode* solve(vector<int>& pre, vector<int>& post, int start, int end){
+        if(start > end || ind >= pre.size()) return NULL;
+
+        TreeNode* root = new TreeNode(pre[ind++]);
+        if(ind >= pre.size() || start == end) return root;
+
+        int p = search(post, start, end, pre[ind]);
+
+        root->left = solve(pre, post, start, p);
+        root->right = solve(pre, post, p+1, end-1);
+
+        return root;
+    }
+    TreeNode* constructFromPrePost(vector<int>& pre, vector<int>& post) {
+        int n = pre.size();
+        return solve(pre, post, 0, n-1);
+    }
+};
