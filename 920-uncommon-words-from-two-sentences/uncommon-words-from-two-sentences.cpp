eea21958251b3bdca16b683b5d549789080060db
Rishita Srivastava <rishitasrivastava04@gmail.com>
2024-09-17 19:04:58 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (89.02%) - LeetSync

---
+++ 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<string> uncommonFromSentences(string s1, string s2) {
+        unordered_map<string, int> m;
+        string word;
+        for(int i=0; i<s1.size(); i++){
+            if(s1[i] != ' '){
+                word.push_back(s1[i]);
+            }
+            if(s1[i] == ' ' ||i == s1.size()-1){
+                m[word]++;
+                word = "";
+            }
+        }
+        for(int i=0; i<s2.size(); i++){
+            if(s2[i] != ' '){
+                word.push_back(s2[i]);
+            }
+            if(s2[i] == ' ' ||i == s2.size()-1){
+                m[word]++;
+                word = "";
+            }
+        }     
+        vector<string> ans;
+        for(auto c:m){
+            if(c.second == 1){
+                ans.push_back(c.first);
+            }
+        }   
+        return ans;
+    }
+};
