f19e467a82e4ed718f8dc5b5c8a0a9c6a0aee846
Rishita Srivastava <rishitasrivastava04@gmail.com>
2024-09-20 23:00:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (43.78%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        if(n==0){
-            return 1;
-        }
-        if(n==1){
-            return x;
-        }
+        if(n==0) return 1;
+        if(n==1) return x;
         if(n<0){
-            n++;
-            return 1/x*myPow(1/x, -n);
+            n++; return 1/x*myPow(1/x, -n);
         }
+
         double ans = myPow(x, n/2);
-        if(n%2==0){
-            return ans*ans;
-        }
-        else{
-            return x*ans*ans;
-        }
+
+        if(n%2==0) return ans*ans;
+        else return x*ans*ans;
+
     }
 };
