1041c5ed6ee807471d1879511b709159bcf579de
Rishita Srivastava <rishitasrivastava04@gmail.com>
2024-09-25 00:54:00 +0530

    Time: 111 ms (5.05%) | Memory: 40 MB (5.02%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
-    void solve(vector<int>& cand, int target,int ind, vector<int> &temp, vector<vector<int>> &ans){
-        if(target==0){
+     void solve(vector<vector<int>>& ans,vector<int> temp,int i,vector<int>& candidates,int &target,int sum){
+        if(sum==target){
             ans.push_back(temp);
             return;
-        }
+        } 
+        if(i == candidates.size() || sum > target) return;
 
-        for(int i=ind; i<cand.size();i++){
-            if(i>ind && cand[i]==cand[i-1] ) continue;
-            if(cand[i]>target) break;
-            temp.push_back(cand[i]);
-            solve(cand, target-cand[i], i+1, temp, ans);
-            temp.pop_back();
-        }
+        temp.push_back(candidates[i]);
+        sum += candidates[i];
+        solve(ans,temp,i+1,candidates,target,sum);
+
+        temp.pop_back();
+        sum -= candidates[i];
+        while(i+1 < candidates.size() && candidates[i] == candidates[i+1]) i++;
+        solve(ans,temp,i+1,candidates,target,sum);
+
     }
+
     vector<vector<int>> combinationSum2(vector<int>& cand, int target) {
         vector<vector<int>> ans;
         vector<int> temp;
-        sort(cand.begin(), cand.end()); //1,1,2,5,6,7,10
-        solve(cand, target, 0, temp, ans);
-        return ans;
+        int sum = 0;
+        sort(cand.begin(),cand.end());
+        solve(ans,temp,0,cand,target,sum);
+        return ans;
+
     }
 };
