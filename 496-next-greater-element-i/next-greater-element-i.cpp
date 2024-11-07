8f031ba2677cc104150e6e65b612b4eeded7e2ee
Rishita Srivastava <rishitasrivastava04@gmail.com>
2024-11-07 19:35:21 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (44.79%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -3,16 +3,20 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         stack<int> st;
         unordered_map<int,int> mp;
-        for(auto i:nums2){
+        for(int i:nums2){
             while(!st.empty() && st.top()<i){
                 mp[st.top()] = i;
                 st.pop();
             }
             st.push(i);
         }
-        for(int i=0;i<nums1.size();i++)
-            nums1[i] = mp.count(nums1[i]) ? mp[nums1[i]] : -1;
-        
-        return nums1;
+        vector<int> ans;
+        for(int i:nums1){
+            if(mp.find(i)!=mp.end()){
+                ans.push_back(mp[i]);
+            }
+            else ans.push_back(-1);
+        }
+        return ans;
     }
 };
