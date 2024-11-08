1503bf2e35d7aff5dc3a33cfe22eb24c570de6f6
Rishita Srivastava <rishitasrivastava04@gmail.com>
2024-11-08 17:05:49 +0530

    Time: 74 ms (8.64%) | Memory: 90.3 MB (17.20%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
-    vector<int> nextSmaller(vector<int>& heights) {
+    vector<int> nse(vector<int>& heights) {
         int n=heights.size(); vector<int> ans(n);
         stack<int> s; s.push(-1);
 
         for(int i=n-1;i>=0;i--){
             int cur = heights[i];
+            //we want next smaller 
+            //so we will pop until we are getting the top as > current
             while(s.top() != -1 && cur<=heights[s.top()]) s.pop();            
             ans[i] = s.top();
             s.push(i);
         }
         return ans;
     };
-        vector<int> prevSmaller(vector<int>& heights ) {
+        vector<int> pse(vector<int>& heights ) {
         int n=heights.size(); vector<int> ans(n);
         stack<int> s; s.push(-1);
 
         for(int i=0;i<n;i++){
             int cur = heights[i];
+            //again we want the prev SMALLER
+            //so we will pop until we are getting a greater element in the stack
             while(s.top() != -1 && cur<=heights[s.top()]) s.pop();
             ans[i] = s.top();
             s.push(i);
@@ -29,8 +33,8 @@ public:
         int n = heights.size();
         int ans=INT_MIN;
         vector<int> prev(n,0), next(n,0);
-        prev = prevSmaller(heights);
-        next = nextSmaller(heights);
+        prev = pse(heights);
+        next = nse(heights);
 
         for(int i=0;i<n;i++){
             if(next[i]==-1) next[i]=n;  
