8e1a2a08e108504d8dbfa0ee7d7dd15e0b46fd6d
Rishita Srivastava <rishitasrivastava04@gmail.com>
2024-11-19 17:04:05 +0530

    Time: 0 ms (100.00%) | Memory: 16.1 MB (90.79%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        int fresh = 0, time = 0;
+        queue<pair<int,int>> q;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                //put rotten in queue
+                if(grid[i][j] == 2){
+                    q.push({i,j});
+                }
+                //count fresh
+                else if(grid[i][j] == 1){
+                    fresh++;
+                }
+            }
+        }
+        if(fresh==0) return 0;
+
+        int delr[] = {-1, 0, 1, 0};
+        int delc[] = {0, 1, 0, -1};
+
+        while(!q.empty()){
+            int temp = 0;
+            int size = q.size();
+            while(size--){
+
+                int r = q.front().first;
+                int c = q.front().second;
+                q.pop();
+
+                for(int i=0; i<4;i++){
+                    int row = r+delr[i];
+                    int col = c+delc[i];
+
+                    //process the fresh oranges
+                    if(row>=0 && row<n && col>=0 && col<m && grid[row][col]==1){
+                        grid[row][col] = 2;
+                        q.push({row, col});
+                        temp++;
+                    }
+                }
+            }
+            //if change happened in this traversal increase the time
+            if(temp != 0) time++;
+        }
+
+        //check if all fresh turned rotten
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1){
+                    time = -1;
+                }
+            }
+        }
+        return time;
+    }
+};
