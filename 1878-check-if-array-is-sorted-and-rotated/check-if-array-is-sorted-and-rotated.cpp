13fc7f0e3481e4b2c1fddf28cdecf74aacf7f001
Rishita Srivastava <rishitasrivastava04@gmail.com>
2025-02-02 10:12:57 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (98.99%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
+        //count the no. of decreasing times
+        //if more than one ... not sorted
         int count=0;
         for(int i=0;i<nums.size();i++){
             if(nums[i]>nums[(i+1)%nums.size()]) count++;
