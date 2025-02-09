d786701556ffeda4dbacf6c18a4e183a3e5eb724
Rishita Srivastava <rishitasrivastava04@gmail.com>
2025-02-09 12:37:15 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (99.07%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -23,7 +23,7 @@ public:
         int delc[] = {0, 1, 0, -1};
 
         while(!q.empty()){
-            int temp = 0;
+            bool flag = false;
             int size = q.size();
             while(size--){
 
@@ -39,12 +39,12 @@ public:
                     if(row>=0 && row<n && col>=0 && col<m && grid[row][col]==1){
                         grid[row][col] = 2;
                         q.push({row, col});
-                        temp++;
+                        flag = true;
                     }
                 }
             }
             //if change happened in this traversal increase the time
-            if(temp != 0) time++;
+            if(flag) time++;
         }
 
         //check if all fresh turned rotten
