c35872bf310f04619a595cf36aaf9e66cdafd7b5
Rishita Srivastava <rishitasrivastava04@gmail.com>
2025-02-28 22:05:02 +0530

    Time: 3 ms (29.54%) | Memory: 22 MB (70.33%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -9,10 +9,14 @@ public:
             else{ // -ve curr value
                 while(!s.empty() && s.top()>0 && s.top()<abs(a[i])) 
                     s.pop(); //stack filled and top +ve and cur -ve bigger
-                if(!s.empty() && s.top()==abs(a[i])) s.pop(); //top and curr equal opp
-                else if(s.empty() || s.top()<0) s.push(a[i]); 
+                    
+                if(!s.empty() && s.top()>0 && s.top()==abs(a[i])) s.pop(); //top and curr equal opp
+                else if(s.empty() || s.top()<0) s.push(a[i]); //push only if the above condition is not true
             }
         }
+
+
+
         vector<int> ans;
         while(!s.empty()){
             ans.push_back(s.top()); s.pop();
