ca3e6c56fc4a4cf1abf993157522de7fcfb736cb
Rishita Srivastava <rishitasrivastava04@gmail.com>
2026-04-03 09:45:17 +0530

    Time: 1 ms (76.55%) | Memory: 14.7 MB (52.86%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -4,9 +4,10 @@ public:
         unordered_map<int,int> mp;
         int n = nums.size();
         for(int i=0;i<n;i++){
-            int j = target - nums[i];
-            if(mp.find(j)!=mp.end()) return {mp[j], i};
-            mp[nums[i]] = i;
+            int temp = target - nums[i];
+            if(mp.find(temp)!=mp.end()) 
+                return {mp[temp], i}; //return index
+            mp[nums[i]] = i; //store index
         }
         return {};
     }
